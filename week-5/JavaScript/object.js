cb596a245e2fc429f16af75874f700ac424ae8dc
Debasish <debasishdas2439@gmail.com>
2025-02-11 22:28:02 +0530

    more on objects and shallow copy

--- week-5/JavaScript/object.js
+++ week-5/JavaScript/object.js
@@ -13,4 +13,25 @@ p1.name = "XYZ";
 console.log(p2);
 console.log(p3);
 
+console.log(typeof(p1.name));
 
+let onj = {
+    fName : "Debasish",
+    lName : "Das",
+    age: 19,
+    address: {
+        house: "abc",
+        dist: "5km"
+    }
+}
+
+let newObj = {
+    ...onj //creating a shallow coppy
+}
+ console.log(newObj);
+ 
+ onj.age = 20;
+
+ onj.address.dist = "8km"
+ console.log(newObj);
+ 
