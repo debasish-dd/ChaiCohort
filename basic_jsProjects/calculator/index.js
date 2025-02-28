5d4aea1d17446faa6259277b7fb087e52e0b9a6d
Debasish <debasishdas2439@gmail.com>
2025-02-28 14:50:14 +0530

    basic calculator done, Some edge casses needed to be handle

--- basic_jsProjects/calculator/index.js
+++ basic_jsProjects/calculator/index.js
@@ -16,12 +16,59 @@ function updateDisplay(val) {
         button.addEventListener('click' , ()=>{
             const buttonText = button.textContent;
 
-            if (button.classList.contains('operator') && buttonText !== '=') {
-                if (buttonText === '.' && currentInput.includes('.')) {
+            if (!button.classList.contains('operator') && buttonText!=='=') {
+                if (buttonText==='.' && currentInput.includes('.')) {
                     return;
                 }
                 currentInput += buttonText;
+                expression += buttonText;
+                updateDisplay(expression);
+
             }
+        else if(button.classList.contains('operator')){
+            if(buttonText==='CLR'){
+                currentInput = '';
+                expression = '';
+                updateDisplay('');
+            }else if(buttonText==='⌫'){
+                currentInput = currentInput.slice(0,-1);
+                expression = expression.slice(0,-1);
+                updateDisplay(expression);    
+            }
+            switch (buttonText) {
+                case '÷':
+                case '×':
+                case '−':
+                case '+':
+                    // Prevent multiple operators in a row
+                    if (currentInput !== '' && !['÷', '×', '−', '+'].includes(expression.slice(-1))) {
+                        currentInput = '';
+                        expression += buttonText;
+                        updateDisplay(expression);
+                    }
+                    break;
+            }
+        }
+
+        if (buttonText==='=') {
+            try {
+                let evalExpresion = currentInput
+                .replace('x' , '*')
+                .replace('%' , '/')
+                .replace('−', '-');
+
+                const result = eval(evalExpresion);
+                if(!isFinite(result)) throw new Error('Invalid calculation');
+                expression = Number(result.toFixed(6)).toString();
+                currentInput = expression;
+                updateDisplay(expression);
+
+            } catch (error) {
+                updateDisplay(error);
+                expression = '';
+                currentInput = '';
+            }
+        }
 
         })
         
@@ -30,3 +77,4 @@ function updateDisplay(val) {
     })
         
 // Initial display
+updateDisplay('');
