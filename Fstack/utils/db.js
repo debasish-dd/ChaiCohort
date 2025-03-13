cca509dad8c614eda1b1bf688cacb42d86de482a
Debasish <debasishdas2439@gmail.com>
2025-03-13 21:20:53 +0530

    conneced to db

--- Fstack/utils/db.js
+++ Fstack/utils/db.js
@@ -1,4 +1,20 @@
 import mongoose from "mongoose"
 
+import dotenv from "dotenv"
 
-mongoose.connect(url)
+dotenv.config()
+
+const db = ()=>{
+
+        mongoose.connect(process.env.MONGO_URL)
+        .then(()=>{
+            console.log("connected to mongoDB");
+    
+        })
+        .catch((err)=>{
+            console.log("error: failed to connect to mongoDB", err);
+            
+        })
+    }
+
+    export default db;
