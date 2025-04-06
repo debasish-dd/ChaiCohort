4c555f98d5f52dc924333b8a52667a8aeaf1f102
Debasish <debasishdas2439@gmail.com>
2025-04-06 17:50:14 +0530

    fixing bugs

--- mega_todo/src/controllers/healthCheck.controller.js
+++ mega_todo/src/controllers/healthCheck.controller.js
@@ -1,4 +1,4 @@
-import {ApiResponse} from "../utils/api-response"
+import {ApiResponse} from "../utils/api-response.js"
 
 const healthCheck = (req, res)=>{
     res.ststus(200).json(new ApiResponse(200, {message: "server is running succesfully"}))
