0683a0d618f7f70ddf04ea8976df18bb4af57aaf
Debasish <debasishdas2439@gmail.com>
2025-06-22 16:31:27 +0530

    react 19

--- React/React-00/src/App.js
+++ React/React-00/src/App.js
@@ -1,16 +1,27 @@
+import React from "https://esm.sh/react@19.1.0"
+import ReactDOM from "https://esm.sh/react-dom@19.1.0/client"
 
 const App = ()=>{
     return React.createElement(
         "div",
-        {},
+        { },
         React.createElement(
             "h1",
-            {},
+            {class : "test"},
             "CHAI CHILL & REACT"
         )
     )
 }
+
+const anotherApp = ()=>{
+    return React.createElement(
+        "h2",
+        {},
+        "This is heading 2"
+    )
+}
 const container = document.getElementById("root")
 
-const root = ReactDom.createRoot(container) 
+const root = ReactDOM.createRoot(container); 
 root.render(React.createElement(App))
+// root.render(React.createElement(anotherApp))
