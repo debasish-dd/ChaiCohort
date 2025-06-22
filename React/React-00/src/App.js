2c58530d27b710f00fee06dbdaf53472bf5ae0ae
Debasish <debasishdas2439@gmail.com>
2025-06-22 22:19:28 +0530

    got another interesting bug

--- React/React-00/src/App.js
+++ React/React-00/src/App.js
@@ -1,27 +1,33 @@
-import React from "https://esm.sh/react@19.1.0"
-import ReactDOM from "https://esm.sh/react-dom@19.1.0/client"
+import React from 'https://esm.sh/react@19.1.0'
+import ReactDOM from 'https://esm.sh/react-dom@19.1.0/client'
 
-const App = ()=>{
-    return React.createElement(
-        "div",
-        { },
-        React.createElement(
-            "h1",
-            {class : "test"},
-            "CHAI CHILL & REACT"
-        )
-    )
+const Chai = props => {
+  return React.createElement('h1', {}, [
+    React.createElement('div', {}, [
+      React.createElement('p', {}, props.name),
+      React.createElement('p', {}, props.price)
+    ])
+  ])
 }
 
-const anotherApp = ()=>{
-    return React.createElement(
-        "h2",
-        {},
-        "This is heading 2"
-    )
+const App = () => {
+  return React.createElement(
+    'div',
+    {},
+    [React.createElement('h1', { class: 'test' }, [
+      'chai list',
+    ]),
+    React.createElement(Chai, { name:'masala chai, price:500' }),
+    React.createElement(Chai, { name:'ginger chai', price:400 }),
+    React.createElement(Chai, { name: 'olong chai', price:700 })]
+  )
 }
-const container = document.getElementById("root")
 
-const root = ReactDOM.createRoot(container); 
+const anotherApp = () => {
+  return React.createElement('h2', {}, 'This is heading 2')
+}
+const container = document.getElementById('root')
+
+const root = ReactDOM.createRoot(container)
 root.render(React.createElement(App))
 // root.render(React.createElement(anotherApp))
