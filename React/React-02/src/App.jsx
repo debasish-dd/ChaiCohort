6c2a4d781aca4368eebb4e210d8d621394cfd42c
Debasish <debasishdas2439@gmail.com>
2025-06-24 20:39:03 +0530

    learnt some vite config & about cors

--- React/React-02/src/App.jsx
+++ React/React-02/src/App.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react"
+
 export function App(){
+    
+    const [message, setMessage] = useState("loading")
+
     return (
         <div>
             <h1>Welcome To React with ChaiCohort</h1>
+            <p>serving chai with react</p>
+            <h2>{message}</h2>
         </div>
     )
 }
--- React/React-02/vite.config.js
+++ React/React-02/vite.config.js
@@ -1,8 +1,14 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig, loadEnv } from 'vite'
 
-
 export default defineConfig({
-    plugins: [react()],
-    
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://localhost:3000',
+        changeOrigin: true
+      }
+    }
+  }
 })
